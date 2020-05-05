81bdc1f8cf293592c00824e47ee03fd118ab542e
Rebecca Siegal <rebecca@Rebeccas-MacBook-Air.local>
2020-05-05 13:02:29 -0400

    updated app.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import AddAnimal from '../AddAnimal/AddAnimal'

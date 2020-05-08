226e5f88a3b0795cad9784cd6fd356d747c15faf
Rebecca Siegal <rebecca@Rebeccas-MacBook-Air.local>
2020-05-08 12:48:18 -0400

    Updated project

--- src/App/App.js
+++ src/App/App.js
@@ -9,125 +9,151 @@ import dummyStore from '../dummy-store'
 import config from '../config'
 import { getNamesForAnimal, findName, findAnimal} from '../names-helper'
 import './App.css'
-
-
+ 
+ 
 class App extends Component {
-  state = {
-   animals: [],
-    names: [],
-  };
-
-  componentDidMount() {
-    // fake date loading from API call
-    setTimeout(() => this.setState(dummyStore), 600)
-    
-  }
-  
-
-  renderNavRoutes() {
-    const { animals, names } = this.state
-    return (
-      <div>
-        {['/', '/animals/animalsid'].map(path =>
-          <Route
-            exact
-            key={path}
-            path={path}
-            render={routeProps =>
-              <Animals
-                animals={animals}
-                names={names}
-                {...routeProps}
-              />
-            }
-          />
-        )}
-        <Route
-          path='/names/:animalId'
-          render={routeProps => {
-            const { animalId } = routeProps.match.params
-            const name = findName(name, animalId) || {}
-            const animal = findAnimal(animal, animalId)
-            return (
-              <Animals
-                {...routeProps}
-                animal={animal}
-              />
-            )
-          }}
-        />
-        <Route
-          path='/add-animal'
-          component={Animals}
-        />
-        <Route
-          path='/add-name'
-          component={Names}
-        />
-      </div>
-    )
-  }
-
-  renderMainRoutes() {
-    const { names, animals } = this.state
-    return (
-      <div>
-        {['/', '/animals/:animalId'].map(path =>
-          <Route
-            exact
-            key={path}
-            path={path}
-            render={routeProps => {
-              const { animalId } = routeProps.match.params
-              const  votes = (votes, animalId)
-              return (
-                <Names
-                  {...routeProps}
-                  votes={votes}
-                />
-              )
-            }}
-          />
-        )}
-        
+ state = {
+  animals: [],
+   names: [],
+ };
+ 
+ componentDidMount() {
+   // fake date loading from API call
+   fetch(`${config.API_ENDPOINT}/names`)
+     .then(res => {
+       if(!res.ok) {
+         throw new Error('Something went wrong, please try again later');
+       }
+       return res.json();
+     })
+     .then(names => {
+       this.setState({names});
+           })
+           .then (()=> {
+             return (fetch(`${config.API_ENDPOINT}/animals`))
+           })
+           .then(res => {
+             if(!res.ok) {
+               throw new Error('Something went wrong, please try again later');
+             }
+             return res.json();
+           })
+           .then (animals =>{
+             this.setState({animals})
+           })
+     .catch(err => {
+       this.setState({
+         error: err.message
+       });
+     });
+ }
+ 
+ renderNavRoutes() {
+   const { animals, names } = this.state
+   return (
+     <div>
+       {['/', '/animals/animalsid'].map(path =>
+         <Route
+           exact
+           key={path}
+           path={path}
+           render={routeProps =>
+             <Animals
+               animals={animals}
+               names={names}
+               {...routeProps}
+             />
+           }
+         />
+       )}
+       <Route
+         path='/names/:animalId'
+         render={routeProps => {
+           const { animalId } = routeProps.match.params
+           const name = findName(name, animalId) || {}
+           const animal = findAnimal(animal, animalId)
+           return (
+             <Animals
+               {...routeProps}
+               animal={animal}
+             />
+           )
+         }}
+       />
+       <Route
+         path='/add-animal'
+         component={Animals}
+       />
+       <Route
+         path='/add-name'
+         component={Names}
+       />
+     </div>
+   )
+ }
+ 
+ renderMainRoutes() {
+   const { names, animals } = this.state
+   return (
+     <div>
+       {['/', '/animals/:animalId'].map(path =>
+         <Route
+           exact
+           key={path}
+           path={path}
+           render={routeProps => {
+             const { animalId } = routeProps.match.params
+             const  votes = (votes, animalId)
+             return (
+               <Names
+                 {...routeProps}
+                 votes={votes}
+               />
+             )
+           }}
+         />
+       )}
       
-        <Route
-          path='/add-animal'
-          component={AddAnimal}
-        />
-        <Route
-          path='/add-name'
-          render={routeProps => {
-            return (
-              <AddName
-                {...routeProps}
-                animals={animals}
-              />
-            )
-          }}
-        />
-      </div>
-    )
-  }
-
-  render() {
-    return (
-      <div className='App'>
-        <nav className='App__nav'>
-          {this.renderNavRoutes()}
-        </nav>
-        <header className='App__header'>
-          <h1>
-            <Link to='/'>Animal Naming and Voting</Link>
-            {' '}
-          </h1>
-        </header>
-        <main className='App__main'>
-          {this.renderMainRoutes()}
-        </main>
-      </div>
-    )
-  }
+       />
+       <Route
+         path='/add-animal'
+         component={AddAnimal}
+       />
+       <Route
+         path='/add-name'
+         render={routeProps => {
+           return (
+             <AddName
+               {...routeProps}
+               animals={animals}
+             />
+           )
+         }}
+       />
+     </div>
+   )
+ }
+ 
+ render() {
+   return (
+     <div className='App'>
+       <nav className='App__nav'>
+         {this.renderNavRoutes()}
+       </nav>
+       <header className='App__header'>
+         <h1>
+           <Link to='/'>Animal Naming and Voting</Link>
+           {' '}
+           <FontAwesomeIcon icon='check-double' />
+         </h1>
+       </header>
+       <main className='App__main'>
+         {this.renderMainRoutes()}
+       </main>
+     </div>
+   )
+ }
 }
-
+ 
 export default App
+
